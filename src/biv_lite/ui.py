84e2b63688a09393bae7b9a4b1246489e1c283b7
k2145129 <k2145129@kcl.ac.uk>
2025-04-01 16:17:23 +0100

    Added UI for frame widget

--- src/biv_lite/ui.py
+++ src/biv_lite/ui.py
@@ -3,6 +3,8 @@ from pathlib import Path
 import pyvista as pv
 from biv_lite.biv_mesh import BivMesh
 from biv_lite.vis import plot_biv_mesh
+from biv_lite.biv_frames import BivFrames
+from biv_lite.utils import to_pyvista_faces
 
 app = typer.Typer(help="User interactive biv-lite application")
 
@@ -12,7 +14,7 @@ def start_ui(input_path: Path = typer.Argument(..., help="Either a fitted model 
     if input_path.is_file():
         ui_biv_mesh(input_path)
     elif input_path.is_dir():
-        raise NotImplementedError
+        ui_biv_frames(input_path)
     else:
         raise FileNotFoundError
 
@@ -48,3 +50,62 @@ def ui_biv_mesh(input_path: Path):
         p.add_text(text=lbl, position=(30, start_pos), font_size=10)
         start_pos += size + (size // 10)
     p.show()
+
+
+def ui_biv_frames(input_path: Path):
+    """Start UI for multiple frames of biv models"""
+    biv_frames = BivFrames.from_folder(input_path)
+    ui = BivMotionUI(biv_frames)
+    ui.plotter.show()
+
+class BivMotionUI:
+    def __init__(self, biv_frames: BivFrames):
+        self.biv_frames = biv_frames
+        self.plotter = pv.Plotter()
+
+        self.current_frame = 0
+        self.actors = plot_biv_mesh(self.biv_frames[self.current_frame], self.plotter)
+        self.plotter.add_slider_widget(self.replace_mesh_callback, [0, len(self.biv_frames) - 1],
+                                       value=self.current_frame, title="Frame",
+                                       fmt="%.0f")
+        self.add_toggle_visibility_widgets()
+
+    def add_toggle_visibility_widgets(self):
+        size = 20
+        start_pos = 10
+
+        for lbl, a in self.actors.items():
+            toggle_vis_callback = SetVisibilityCallback(a)
+            self.plotter.add_checkbox_button_widget(
+                callback=toggle_vis_callback,
+                value=True,
+                color_on=a.prop.color,
+                color_off='white',
+                size=size,
+                border_size=1,
+                position=(5.0, start_pos)
+            )
+            self.plotter.add_text(text=lbl, position=(30, start_pos), font_size=10)
+            start_pos += size + (size // 10)
+
+    def replace_mesh_callback(self, value):
+        if int(round(value)) == self.current_frame:
+            return
+
+        self.current_frame = int(round(value))
+
+        # replace LV
+        lv = self.biv_frames[self.current_frame].lv_endo()
+        mesh = pv.PolyData(lv.nodes, to_pyvista_faces(lv.elements))
+        self.actors['LV'].mapper.dataset.copy_from(mesh)
+
+        # replace RV
+        rv = self.biv_frames[self.current_frame].rv_endo()
+        mesh = pv.PolyData(rv.nodes, to_pyvista_faces(rv.elements))
+        self.actors['RV'].mapper.dataset.copy_from(mesh)
+
+        # replace EPI
+        epi = self.biv_frames[self.current_frame].rvlv_epi()
+        mesh = pv.PolyData(epi.nodes, to_pyvista_faces(epi.elements))
+        self.actors['EPI'].mapper.dataset.copy_from(mesh)
+
